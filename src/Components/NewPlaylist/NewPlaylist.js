405055a646b6e2ff39cf12763292e7631f519a3c
craigselbyio <craigselby@att.net>
2020-06-07 14:36:31 -0400

    Trim playlist track name if length > 20

--- src/Components/NewPlaylist/NewPlaylist.js
+++ src/Components/NewPlaylist/NewPlaylist.js
@@ -4,18 +4,18 @@ import "./NewPlaylist.css";
 const NewPlaylist = ({ Playlist, removePlaylistItem }) => {
   return (
     <div className="new-playlist">
+      <button id="save-playlist">SAVE</button>
       <input
         className="playlist-name-input"
         placeholder="Name me, so i know its real_"
       />
-      <button id="save-playlist">SAVE</button>
       <div className="playlist-items">
         {Playlist.map((track) => (
           <div className="playlist-item">
             <div className="playlist-item-remove" onClick={() => removePlaylistItem(track)}></div>
             <img src={track.img} alt="" />
-            <h5 className="playlist-item-name">{track.name}</h5>
-            <h6 className="playlist-item-artist">{track.artist}</h6>
+            <h5 className="playlist-item-name">{ track.name.length > 20 ? `${track.name.substring(0,20)}...` : track.name }</h5>
+            <h6 className="playlist-item-artist">{ track.artist }</h6>
           </div>
         ))}
       </div>

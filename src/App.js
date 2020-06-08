9ba7ef3a02f9adec6ddd1c9c49ee0a6c41a9af5c
craigselbyio <craigselby@att.net>
2020-06-08 15:25:14 -0400

    Add useEffect hook, conditionally render with btns

--- src/App.js
+++ src/App.js
@@ -1,31 +1,34 @@
 import React, { useState, useEffect } from "react";
-import logo from "./wave-logo.png";
+import logo from "./img/wave-logo.svg";
 import "./App.css";
 import Featured from "./Components/Featured/Featured";
 import { Spotify } from "./util/Spotify";
-import NewPlaylist from './Components/NewPlaylist/NewPlaylist';
+import NewPlaylist from "./Components/NewPlaylist/NewPlaylist";
 import motorsport from "./img/motorsport-migos-nickiminaj-cardib.png";
 import Search from "./Components/Search/Search";
 
 function App() {
+  const [newReleases, setNewReleases] = useState([]);
 
-const [newReleases, setNewReleases ] = useState([]);
+  const [newPlaylist, setNewPlaylist] = useState([]);
 
-const [newPlaylist, setNewPlaylist ] = useState([]);
+  const [searchResults, setSearchResults] = useState([]);
 
-const [searchResults, setSearchResults] = useState([]);
+  const [homeView, setHomeView] = useState("new");
 
-async function searchGetNew() {
-    let nr = await Spotify.getNew();
-    console.log(nr)
-    if(nr.error) {
-      console.log(nr.error)
-    } else {
-      setNewReleases(nr);
-      console.log(newReleases)
-    }
-  }
-  
+  useEffect(() => {
+    const getNewReleases = async () => {
+      let response = await Spotify.getNew();
+
+      if (response.error) {
+        console.log(response.error);
+      } else {
+        setNewReleases(response);
+      }
+    };
+    getNewReleases();
+  }, []);
+
   const trackSearch = async (searchTerm) => {
     try {
       let searchResults = await Spotify.trackSearch(searchTerm);
@@ -34,36 +37,58 @@ async function searchGetNew() {
     } catch (error) {
       console.log(error);
     }
-  }
-  
+  };
+
   const handlePlaylistAdd = (track) => {
-    let newTrack = {...track,
-    "inPlaylist": true}
+    let newTrack = { ...track, inPlaylist: true };
     setNewPlaylist([...newPlaylist, newTrack]);
 
-    let matchingTrack = newReleases.find(newReleaseTrack => newReleaseTrack.id === track.id) || searchResults.find(newReleaseTrack => newReleaseTrack.id === track.id) ;
+    let matchingTrack =
+      newReleases.find((newReleaseTrack) => newReleaseTrack.id === track.id) ||
+      searchResults.find((newReleaseTrack) => newReleaseTrack.id === track.id);
     matchingTrack.inPlaylist = true;
-
-    console.log(matchingTrack);
-  }
+  };
 
   const handlePlaylistRemove = (track) => {
-    let updatedPlaylist = newPlaylist.filter(playlistTrack => playlistTrack.id !== track.id);
-    setNewPlaylist([...updatedPlaylist])
-  }
+    let updatedPlaylist = newPlaylist.filter(
+      (playlistTrack) => playlistTrack.id !== track.id
+    );
+    setNewPlaylist([...updatedPlaylist]);
+  };
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
-      <button onClick={searchGetNew}>Search</button>
 
-      {newPlaylist.length > 0 ? <NewPlaylist Playlist={newPlaylist} removePlaylistItem={handlePlaylistRemove} /> : null}
+      <button className={`home-view-btn ${homeView === 'new' && 'home-view-btn-active'}`} onClick={() => setHomeView("new")}>New</button>
+      <button className={`home-view-btn ${homeView === 'search' && 'home-view-btn-active'}`} onClick={() => setHomeView("search")}>Search</button>
 
-      <Search addToPlaylist={handlePlaylistAdd} newPlaylist={newPlaylist} trackSearch={trackSearch} searchResults={searchResults} />
+      {newPlaylist.length > 0 ? (
+        <NewPlaylist
+          Playlist={newPlaylist}
+          removePlaylistItem={handlePlaylistRemove}
+        />
+      ) : null}
 
-      {searchResults.length > 0 ? null : <Featured addToPlaylist={handlePlaylistAdd} newReleases={newReleases} newPlaylist={newPlaylist} /> }
+      {homeView === "search" && (
+        <Search
+          addToPlaylist={handlePlaylistAdd}
+          newPlaylist={newPlaylist}
+          trackSearch={trackSearch}
+          searchResults={searchResults}
+        />
+      )}
+
+      {homeView === "new" && (
+        <Featured
+          addToPlaylist={handlePlaylistAdd}
+          newReleases={newReleases}
+          newPlaylist={newPlaylist}
+        />
+      )}
+
     </div>
   );
 }

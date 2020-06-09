d766adbc8bd9d54062dab6ef49d7bf015196e024
craigselbyio <craigselby@att.net>
2020-06-08 23:08:41 -0400

    Add onClick to #1 featured img

--- src/Components/Featured/Featured.js
+++ src/Components/Featured/Featured.js
@@ -2,8 +2,12 @@ import React from "react";
 import "./featured.css";
 import Track from "../Track/Track";
 
-const Featured = ({ newReleases, newPlaylist, addToPlaylist, isInPlaylist }) => {
-
+const Featured = ({
+  newReleases,
+  newPlaylist,
+  addToPlaylist,
+  isInPlaylist,
+}) => {
   return (
     <div className="featured-wrap">
       <div className="number-one-track">
@@ -11,32 +15,35 @@ const Featured = ({ newReleases, newPlaylist, addToPlaylist, isInPlaylist }) => 
           (track, index) =>
             index === 0 && (
               <>
-               <img
-                    className="number-one-track-img"
-                    src={track.img}
-                    alt=""
-                  />
-                <div className="number-one-track-info-wrap">
-                <div className="number-one-track-info">
-                  <h2 className="number-one-track-title">{track.name}</h2>
-                  <h4 className="number-one-track-artist">{track.artist}</h4>
-                </div>
-                <button
+                <img
                   onClick={() => addToPlaylist(track)}
-                  className={
-                    isInPlaylist(track.id)
-                      ? "add-or-remove-track-btn track-in-playlist"
-                      : "add-or-remove-track-btn"
-                  }
-                  disabled={isInPlaylist(track.id) ? "disabled" : ""}
-                ></button>
+                  className={isInPlaylist(track.id) ? 'number-one-track-img track-in-playlist': 'number-one-track-img'}
+                  src={track.img}
+                  alt=""
+                />
+                <div className="number-one-track-info-wrap">
+                  <div className="number-one-track-info">
+                    <h2 className="number-one-track-title">{track.name}</h2>
+                    <h4 className="number-one-track-artist">{track.artist}</h4>
+                  </div>
+                  <button
+                    onClick={() => addToPlaylist(track)}
+                    className={
+                      isInPlaylist(track.id)
+                        ? "add-or-remove-track-btn track-in-playlist"
+                        : "add-or-remove-track-btn"
+                    }
+                    disabled={isInPlaylist(track.id) ? "disabled" : ""}
+                  ></button>
                 </div>
               </>
             )
         )}
       </div>
       <div className="featured-tracks">
-        <h4 className="featured-track-top-text">Click on Track to Create a Playlist</h4>
+        <h4 className="featured-track-top-text">
+          Click on Track to Create a Playlist
+        </h4>
         {newReleases.map((track, index) =>
           index !== 0 ? (
             <Track

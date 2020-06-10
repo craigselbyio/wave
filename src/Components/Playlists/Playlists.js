8bb3bfbc7c48bc29526630c2ad382ff92cabb69b
craigselbyio <craigselby@att.net>
2020-06-10 16:16:58 -0400

    Create Playlist component

---
+++ src/Components/Playlists/Playlists.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react';
+import './Playlist.css';
+import { Spotify } from '../../util/Spotify';
+import noCover from '../../img/no-album-cover.svg';
+
+
+const Playlists = () => {
+
+    const [playlists , setPlaylists] = useState();
+
+    useEffect(() => {
+        const getUsersPlaylists = async () => {
+            let playlists = await Spotify.getUsersPlaylists();
+            setPlaylists([...playlists]);
+            console.log(playlists);
+        }
+        getUsersPlaylists();
+    }, [])
+
+    return (
+        <div className="playlists">
+            {playlists ? (
+            playlists.map(playlist => (
+                <a href={playlist.spotifyLink} target="new">
+                <div className="playlist">
+                    <img className="playlist-img" src={playlist.img ? playlist.img : noCover} alt="" />
+                    <h4>{playlist.name}</h4>
+                </div>
+                </a>
+            ))
+            ) : <h1>No Playlists.</h1>}
+        </div>
+    )
+
+}
+
+export default Playlists;

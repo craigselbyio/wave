6d673c2e966f94ae37fd5a510c25351cc06a50d2
craigselbyio <craigselby@att.net>
2020-06-29 10:30:50 -0400

    Move MusicKit.configure from App.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,19 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+window.MusicKit.configure({
+  developerToken:
+    "eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IjI2UkhQQ0s0M0MifQ.eyJpYXQiOjE1OTMyMDEyNzQsImV4cCI6MTYwODc1MzI3NCwiaXNzIjoiTVA5NVI4VVZUNyJ9.Q1NgSmaiaGprYi1wEN24hL31L-xcCAUmKxKvLaYi3EcYzgfH6CyezJH0LHIzQtyDwxfta4E9Zg5f8QYTLgaxZg",
+  app: {
+    name: "wave App",
+    build: "1",
+  },
+  declarativeMarkup: true,
+});
+
+
+//let music = window.MusicKit.getInstance();
+
 ReactDOM.render(
   <React.StrictMode>
     <App />

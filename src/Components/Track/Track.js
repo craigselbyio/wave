9e31c7005a7394bc4147f88295584dc947858222
craigselbyio <craigselby@att.net>
2020-07-15 19:36:04 -0400

    Remove getSizedImageURL, pass as prop

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -1,15 +1,11 @@
 import React from "react";
-import "./track.css";
+import "./Track.css";
 
-const Track = ({ addToMusicQueue, trackType, trackLocation, track, addToPlaylist, isInPlaylist }) => {
-
-  const getSizedImageURL = (url, size) => {
-    return url.replace(/{[wh]}/g, size);
-  }
+const Track = ({ addToMusicQueue, getSizedImageURL, track, addToPlaylist, isInPlaylist }) => {
 
   return (
     <div onClick={() => addToMusicQueue(track)} className="track">
-      <img className={isInPlaylist(track.id) ? "track-info-img track-in-playlist" : "track-info-img"} src={getSizedImageURL(track.attributes.artwork.url, "600")} alt="" />
+      <img className={isInPlaylist(track.id) ? "track-info-img track-in-playlist" : "track-info-img"} src={getSizedImageURL(track.attributes.artwork.url, "400")} alt="" />
 
       <div className="track-info">
         <h2 className="track-title">{track.attributes.name.length > 20 ? `${track.attributes.name.substring(0,20)}...` : track.attributes.name }</h2>
